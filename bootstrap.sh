336d997aa8208af46720308afa6ed90c2e32af4c
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-04-12 01:19:32 -0400

    add elk dls

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+# install basics
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get install oracle-java8-installer
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+
+# install logstash
+curl -O https://download.elastic.co/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb
+sudo dpkg --install logstash_1.4.2-1-2c0f5a1_all.deb
+# start?
+
+# install elasticsearch
 curl -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.1.deb
 sudo dpkg --install elasticsearch-1.5.1.deb 
 sudo service elasticsearch start
+
+# install kibana
+curl -O https://download.elastic.co/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
+sudo dpkg --install kibana-4.0.1-linux-x64.tar.gz
+# start?
